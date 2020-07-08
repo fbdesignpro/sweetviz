462174151193daae90e3069a71b5894b366e45e0
chadaeschliman <chad.aeschliman@spensatech.com>
2020-07-08 14:31:14 -0400

    Fix non_zeros for non-numeric variables in series_analyzer

--- sweetviz/series_analyzer.py
+++ sweetviz/series_analyzer.py
@@ -41,7 +41,10 @@ def add_series_base_stats_to_dict(series: pd.Series, counts: dict, updated_dict:
     updated_dict["base_stats"] = dict()
     base_stats = updated_dict["base_stats"]
     num_total = counts["num_rows_total"]
-    num_zeros = series[series == 0].sum()
+    try:
+        num_zeros = series[series == 0].sum()
+    except TypeError:
+        num_zeros = 0
     non_nan = counts["num_rows_with_data"]
     base_stats["total_rows"] = num_total
     base_stats["num_values"] = NumWithPercent(non_nan, num_total)
